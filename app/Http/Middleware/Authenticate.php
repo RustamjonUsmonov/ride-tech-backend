edc30bb33e18ba617d5e466d29a4bd6349a53132
RustamjonUsmonov <rustam190401@mail.ru>
2025-08-31 23:25:25 +0500

    fix : code style issues

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
-    protected function redirectTo($request): ?string
+    protected function redirectTo(Request $request): ?string
     {
         if (!$request->expectsJson()) {
             abort(401, 'Unauthenticated.');
--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
@@ -57,7 +58,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
      */
     protected function gate(): void
     {
-        Gate::define('viewTelescope', function ($user) {
+        Gate::define('viewTelescope', function (User $user) {
             return in_array($user->email, [
                 //
             ]);
